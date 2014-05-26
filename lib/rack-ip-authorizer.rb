e4cf17f5fcfcf851f3b8a2550f7f75bc3239db97
telzamek <thibault.elzamek@c4mprod.com>
2014-05-26 13:02:44 +0200

    [FIX] variable scope

--- lib/rack-ip-authorizer.rb
+++ lib/rack-ip-authorizer.rb
@@ -4,16 +4,14 @@ require 'yaml'
 if defined? Rails
   case Rails::VERSION::MAJOR
     when 3
-      path = File.join(File.expand_path('config'), '/ip_authorizations.yml')
-
-      if File.exists?(path)
-        ip_authorizations_by_path = YAML.load_file(path)
-      else
-        raise "config/ip_authorizations.yml is missing"
-      end
-
       class Rack::IpAuthorizer::Railtie < Rails::Railtie
         initializer('rack-ip-authorizer.append') { |app|
+          path = File.join(File.expand_path('config'), '/ip_authorizations.yml')
+          if File.exists?(path)
+            ip_authorizations_by_path = YAML.load_file(path)
+          else
+            raise "config/ip_authorizations.yml is missing"
+          end
           app.config.middleware.insert_after(ActionDispatch::RemoteIp, Rack::IpAuthorizer, ip_authorizations_by_path)
         }
       end
